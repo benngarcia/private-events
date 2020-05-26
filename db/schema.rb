b6ac7f3350e11b6e85404fd37754409e35a3ae7b
benngarcia <beng4606@gmail.com>
2020-05-26 15:02:31 -0400

    Added Event Foreign Key to User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_185717) do
+ActiveRecord::Schema.define(version: 2020_05_26_190153) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_185717) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "event_id"
   end
 
 end
